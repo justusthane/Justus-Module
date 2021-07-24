7874bab25956feb3e6312415f5babcb6b517cd4a
jbadergr (Justus Grunow) <jbadergr@confederationcollege.ca>
2021-07-24 16:55:40 -0400

    Added documentation to Get-IPInfo

--- Get-IPInfo.ps1
+++ Get-IPInfo.ps1
@@ -1,6 +1,14 @@
 function Get-IPInfo {
+<#
+  .Synopsis
+  Gets info about the provided IP address from ipinfo.info
+
+  .Description
+  This cmdlet is a very simple wrapper that curls ipinfo.io for the specified IP address. If no IP address is specified using the -IPAddress parameter, then it returns info about your IP address.
+  #>
  param (
-   $IPAddress
+   # Specify the IP address
+   [string]$IPAddress
  )
  curl https://ipinfo.io/$IPAddress | ConvertFrom-Json
 }
