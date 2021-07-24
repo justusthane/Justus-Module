138f8cf2ac7ed12529f2f792c8148603bb7fcf63
jbadergr (Justus Grunow) <jbadergr@confederationcollege.ca>
2021-07-24 12:18:43 -0400

    Updated documentation

--- Find-ADUser.ps1
+++ Find-ADUser.ps1
@@ -2,10 +2,29 @@
   .Synopsis
   Find-ADUser is a wrapper for Get-ADUser which searches a set of attributes using a single search string.
 
-  By default, only a select subset of properties are returned
   
   .Description
-  A description
+  By default, only a select subset of properties are returned. Use the -Properties parameter to specify which properties to return, or * for all.
+
+  .Example
+  Find-ADUser jbad
+
+  Search for a user by part of their name (not that wildcards are not necessary)
+
+  .Example
+  Find-ADUser -SearchString jbad -Properties DisplayName,SAMAccountName,MemberOf
+
+  Specify which properties to return
+  .Example 
+  Find-ADUser shurget@confederation
+
+  DisplayName              Name     SAMAccountName
+  -----------              ----     --------------
+  jbadergr (Justus Grunow) jbadergr jbadergr
+
+  Because the command also searches proxyAddresses, it can be used to find out what user a specific email address is attached to:
+
+
 #>
 function Find-ADUser {
   [CmdletBinding()]
@@ -13,6 +32,8 @@ function Find-ADUser {
     [Parameter(Mandatory)]
     # Specify the search string
     [string]$SearchString,
+    # Specify which properties to return, or * for all
+    # 
     [array]$Properties = ("DisplayName","Name","SAMAccountName")
   )
   $searchAttributes = "DisplayName -like '*$searchString*' `
